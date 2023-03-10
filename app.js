60040259a63f5e9f3565496374acba3daaf4ed83
Kundnkr <104859784+Kundnkr@users.noreply.github.com>
2023-03-10 22:44:55 +0530

    second

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ app.post('/login', async (req, res) => {
         res.status(400).json({ error });
     }
 })
-let port = 3000;
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`server is runnig at port ${port}`);
 });
